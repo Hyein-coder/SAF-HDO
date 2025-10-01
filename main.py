2df8e0c14fbb4d4e0c8462c17027802624ea623b
Hyein <976hannah@snu.ac.kr>
2025-10-01 17:16:53 +0900

    Use the past data if provided

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ import numpy as np
 import datetime
 import matplotlib.pyplot as plt
 
-aspen_path = "D:\\SAF-HDO py\\250926_pyrolysis_oil_CC_2.apw"
-# aspen_path = "D:/saf_hdo/aspen/250926_pyrolysis_oil_CC.apw"
 case_target = "a"
+# past_path = None
+past_path = r"D:\saf_hdo\results\a_20251001_165004.xlsx"
+# aspen_path = "D:\\SAF-HDO py\\250926_pyrolysis_oil_CC_2.apw"
+aspen_path = "D:/saf_hdo/aspen/250926_pyrolysis_oil_CC_rxn_index.apw"
+
+
 sim = AspenSim(aspen_path, case_target=case_target) # , case_target='j'
+save_dir = os.path.join(os.getcwd(), "results")
 file_tag = case_target + "_" + datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
 
 cc = sim.get_carbon_number_composition(sim.prod_stream)
@@ -22,16 +27,28 @@ domain = [
 ]
 r_now = sim.get_rxn_coefficients()
 
+excel_path = os.path.join(save_dir, f"{file_tag}.xlsx")
+
+var_names = []
+for i, ris in enumerate(sim.rxn_indices):
+    for ii, rxn_idx in enumerate(ris):
+        var_names.append(f"R{i+1}_rxn{rxn_idx}")
+var_names.append("OBJ")
+for n in sim.carbon_number_to_component.keys():
+    var_names.append(f"Prod_C{n}")
+
 def hdo_prod_mse(x):
     sim.apply_rxn_coefficients(x)
     res = sim.get_carbon_number_composition(sim.prod_stream)
     mse = sum((sim.target[n] - res[n])**2 for n in sim.target.keys()) * 10
     print(mse)
-    return mse
+
+    optional_res = np.array(list(res.values())).reshape(1, -1)
+    return mse, optional_res
 hdo_prod_mse(r_now)
 
 #%%
-evaluator = CustomEvaluator(hdo_prod_mse)
+evaluator = CustomEvaluator(hdo_prod_mse, excel_path, var_names)
 objective = GPyOpt.core.task.SingleObjective(evaluator.evaluate)
 r_init = np.array(r_now)
 
@@ -46,37 +63,45 @@ domain_space = GPyOpt.Design_space(space=domain)
 
 acquisition_optimizer = GPyOpt.optimization.AcquisitionOptimizer(domain_space)
 acquisition = GPyOpt.acquisitions.AcquisitionEI(model, domain_space, optimizer=acquisition_optimizer)
-evaluator = GPyOpt.core.evaluators.Sequential(acquisition)
+bo_evaluator = GPyOpt.core.evaluators.Sequential(acquisition)
 
 # Set Bayesian optimization
-BO = ModularBayesianOptimization(model, domain_space, objective, acquisition, evaluator, r_init)
+if past_path is None:
+    BO = ModularBayesianOptimization(model, domain_space, objective, acquisition, bo_evaluator, r_init)
+else:
+    past_data = evaluator.read_past_results(past_path)
+    nx = r_init.shape[1]
+    X_past = np.array(past_data.iloc[:, :nx])
+    obj_past = np.array(past_data.iloc[:, nx]).reshape(-1,1)
+    BO = ModularBayesianOptimization(model, domain_space, objective, acquisition, bo_evaluator, X_past, Y_init=obj_past)
 
 max_time = None
-max_iter = 5   # 100, 200, 500
+max_iter = 2   # 100, 200, 500
 tolerance = -np.inf
 
 # Run Bayesian optimization
 BO.run_optimization(max_iter=max_iter, max_time=max_time, eps=tolerance, verbosity=False)
-BO.plot_convergence(f'res_{file_tag}_convergence.png')
+BO.plot_convergence(os.path.join(save_dir, f'{file_tag}_convergence.png'))
 # plt.savefig(f'res_{file_tag}_convergence.png')
+evaluator.save_best_result()
 #%%
 optimal_rxn_coef = BO.x_opt
 print("==============Optimal Result==============")
 print("Rxn Coefficients for the First Reactor: ")
 print(optimal_rxn_coef)
-np.savetxt(f'res_{file_tag}_optimal_coeff.txt', optimal_rxn_coef)
+np.savetxt(os.path.join(save_dir, f'{file_tag}_optimal_coeff.txt'), optimal_rxn_coef)
 
 optimal_result = hdo_prod_mse(optimal_rxn_coef.reshape(1, -1))
 print(f"Mean Squared Error: {optimal_result}")
 
 cc = sim.get_carbon_number_composition(sim.prod_stream)
-with open(f'res_{file_tag}_carbon_number_composition.txt', 'w') as f:
+with open(os.path.join(save_dir, f'{file_tag}_carbon_number_composition.txt'), 'w') as f:
     f.write(str(cc))
 fig, ax = plt.subplots(1, 1, figsize=(10, 10))
 plt.plot(cc.keys(), cc.values(), '-', label='Simulation')
 plt.plot(sim.target.keys(), sim.target.values(), 'o', label='Experimental Data')
 plt.legend()
-plt.savefig(f'res_{file_tag}_composition.png')
+plt.savefig(os.path.join(save_dir, f'{file_tag}_composition.png'))
 plt.show()
 #%%
 sim.terminate()
--- utils.py
+++ utils.py
@@ -1,12 +1,47 @@
-
+import pandas as pd
+import numpy as np
+import os
 
 class CustomEvaluator(object):
-    def __init__(self, obj_fun):
+    def __init__(self, obj_fun, excel_path, var_names):
         self.iteration_count = 0  # 반복 횟수 카운터 초기화
         self.obj_fun = obj_fun
 
+        self.excel_path = excel_path
+        self.row_names = ["Iter"] + var_names
+        self.data = pd.DataFrame(columns=self.row_names)
+
     def evaluate(self, x):
         print(f"현재 BO Iteration: {self.iteration_count}")
-        val = self.obj_fun(x)
+        val, optional_res = self.obj_fun(x)
+
+        data_row = np.concatenate(([[self.iteration_count]], x, [[val]], optional_res), axis=1)
+        self.data = self.data.append(pd.DataFrame(data_row, columns=self.row_names), ignore_index=True)
+
+        writer_options = self._get_writer_options()
+        with pd.ExcelWriter(self.excel_path, **writer_options) as writer:
+            self.data.to_excel(writer, index=False, sheet_name="BO_Result")
+
         self.iteration_count += 1
         return val
+
+    def save_best_result(self):
+        best_row = self.data.nsmallest(1, "OBJ")
+        writer_options = self._get_writer_options()
+        with pd.ExcelWriter(self.excel_path, **writer_options) as writer:
+            best_row.to_excel(writer, index=False, sheet_name="Best_Result")
+
+    def read_past_results(self, past_path):
+        try:
+            with pd.ExcelFile(past_path) as xls:
+                df = pd.read_excel(xls, sheet_name="BO_Result")
+            return df
+        except Exception:
+            return pd.DataFrame(columns=self.row_names)
+
+    def _get_writer_options(self):
+        if os.path.exists(self.excel_path):
+            writer_options = dict(mode='a', engine='openpyxl', if_sheet_exists='replace')
+        else:
+            writer_options = {}
+        return writer_options
