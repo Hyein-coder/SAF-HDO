0bec5a8b17d7c2db94a4ed2fda5ac0bfed0c46a4
Hyein <976hannah@snu.ac.kr>
2025-11-19 16:09:26 +0900

    Using another case (f) for peak shift

--- sensitivity_analysis.py
+++ sensitivity_analysis.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 save_dir = os.path.join(os.getcwd(), 'results')
 simA = AspenSim(r"D:\saf_hdo\aspen\251023_pyrolysis_oil_CC_case_a_rxn_index.bkp")
 simI = AspenSim(r"D:\saf_hdo\aspen\251111_pyrolysis_oil_CC_case_i_indexing.bkp", case_target="i")
-simK = AspenSim(r"D:\saf_hdo\aspen\251023_pyrolysis_oil_CC_case_k_rxn_enabled.bkp", case_target="k")
+simK = AspenSim(r"D:\saf_hdo\aspen\251023_pyrolysis_oil_CC_case_f.bkp", case_target="f")
 
 sims = [simA, simI, simK]
 original_coefs = []
@@ -28,14 +28,18 @@ def convert_coef(simX):
 names = [s.case_target for s in sims]
 coefs = [convert_coef(s) for s in sims]
 
+#%%
 fig, axs = plt.subplots(1, 2, figsize=(8, 5))
 ax_hdo = axs[0]
 ax_coef = axs[1]
-linestyles = ['-o', '-s', '-^']
-for n, c, ls in zip(names, coefs, linestyles):
+colors = ['#428bca', '#d9534f', '#5cb85c']
+markers = ['o', 's', '^']
+for n, c, cl, mk in zip(names, coefs, colors, markers):
     hdo_res = sim_main.apply_rxn_coefficients(c)
-    ax_hdo.plot(hdo_res.keys(), hdo_res.values(), ls, label=n)
-    ax_coef.plot(range(len(c[0])), c[0], ls, label=n)
+    _, hdo_target = sim_main.set_target(n)
+    ax_hdo.plot(hdo_res.keys(), hdo_res.values(), label=n+" (sim)", color=cl)
+    ax_hdo.plot(hdo_target.keys(), hdo_target.values(), mk, label=n+" (exp)", color=cl)
+    ax_coef.plot(range(len(c[0])), c[0], label=n, color=cl, marker=mk)
 
 ax_hdo.set_title("HDO Product Composition")
 ax_hdo.set_xlabel("Carbon Lengths")
@@ -57,7 +61,24 @@ def rxn_coef_interp(x, y):
 
 hdo_orig = sim_main.apply_rxn_coefficients(coefs[0])
 
-# coarse_test = [0.7]
+#%% Show the case data coverage
+import matplotlib.cm as cm
+
+targets = {}
+for a in ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']:
+    _, tt = simK.set_target(a)
+    targets[a] = tt
+cmap_target = cm.get_cmap('viridis', len(targets))
+
+fig, ax = plt.subplots(1, 1)
+for i, (key, val) in enumerate(targets.items()):
+    plt.plot(val.keys(), val.values(), 'o', markersize=3, label=key,
+             color=cmap_target(i))
+plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+plt.tight_layout()
+plt.show()
+
+#%%
 coarse_test = [0.1, 0.4, 0.7, 1.0]
 hdo_vary_x = []
 hdo_vary_y = []
@@ -65,26 +86,37 @@ sensitivity_dir = os.path.join(r"D:\saf_hdo\aspen", 'sensitivity')
 os.makedirs(sensitivity_dir, exist_ok=True)
 for i in coarse_test:
     coef_vary_x = rxn_coef_interp(i, 0)
-    coef_vary_y = rxn_coef_interp(0, i)
-
     hdo_vary_x.append(sim_main.apply_rxn_coefficients(coef_vary_x))
     sim_main.save_simulation_as(os.path.join(sensitivity_dir, f'caseI_{i:.1f}.bkp'))
 
+for i in coarse_test:
+    coef_vary_y = rxn_coef_interp(0, i)
     hdo_vary_y.append(sim_main.apply_rxn_coefficients(coef_vary_y))
     sim_main.save_simulation_as(os.path.join(sensitivity_dir, f'caseK_{i:.1f}.bkp'))
 
+#%%
 fig, axs = plt.subplots(1, 2, figsize=(8,5))
-linestyles = ['-o', '-s', '-^', '-x', '-d']
+cmap_coarse = cm.get_cmap('viridis', len(coarse_test)+1)
+
 for ax, hdo in zip(axs, [hdo_vary_x, hdo_vary_y]):
-    for d, ls in zip([hdo_orig] + hdo, linestyles):
-        ax.plot(d.keys(), d.values(), ls)
+    for i, (key, val) in enumerate(targets.items()):
+        ax.plot(val.keys(), val.values(), 'o', markersize=3, label=key,
+                 color=cmap_target(i))
+    for idx, d in enumerate([hdo_orig] + hdo):
+        ax.plot(d.keys(), d.values(), '-', color=cmap_coarse(idx))
     ax.grid()
 plt.show()
 
-#%%
-coef_save = rxn_coef_interp(0.3, 0.3)
-sim_main.apply_rxn_coefficients(coef_save)
-# sim_main.save_simulation_as('test_simulation')
-initial_path = os.path.abspath(sim_main.aspen_path)
-new_path = os.path.join(os.path.dirname(initial_path), "test_simulation" + '.bkp')
-sim_main.aspen.SaveAs(new_path)
+#%% Matching case f
+# res = sim_main.get_carbon_number_composition(sim_main.prod_stream)
+#
+# fig, ax = plt.subplots(1, 1)
+# for i, (key, val) in enumerate(targets.items()):
+#     if key == 'f':
+#         plt.plot(val.keys(), val.values(), 'o', markersize=3, label=key,
+#                  color=cmap_target(i))
+# plt.plot(res.keys(), res.values(), '-', color='r')
+# ax.grid()
+# plt.legend(loc='upper left', bbox_to_anchor=(1, 1))
+# plt.tight_layout()
+# plt.show()
